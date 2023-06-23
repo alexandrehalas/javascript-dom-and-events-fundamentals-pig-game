d859b9313ac85430ff4d9d6d9adcaebda3a63a6b
Alexandre Halas <alexandrehalas@gmail.com>
2023-06-23 10:55:19 -0300

    selected some elements and initialize its values

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const diceElement = document.querySelector('.dice');
+
+// starting conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
