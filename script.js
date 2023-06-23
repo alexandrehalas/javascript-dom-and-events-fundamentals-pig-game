613c94b606b9ecaeb54466cc47baca94ce571891
Alexandre Halas <alexandrehalas@gmail.com>
2023-06-23 11:30:17 -0300

    Generate random dice roll and display dice

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 'use strict';
 
 // elements
+
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
+const currentScore0Element = document.querySelector('#current--0');
+const currentScore1Element = document.querySelector('#current--1');
 const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// functions
+
+const hideDice = function () {
+  diceElement.classList.add('hidden');
+};
+
+const generateRandomDiceRoll = function () {
+  return Math.trunc(Math.random() * 6) + 1;
+};
+
+const displayDice = function (diceNumber) {
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${diceNumber}.png`;
+};
 
 // starting conditions
+
 score0Element.textContent = 0;
 score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+hideDice();
+
+// buttons
+
+btnRoll.addEventListener('click', function () {
+  const diceNumber = generateRandomDiceRoll();
+  displayDice(diceNumber);
+});
